3dfd9f9d93942639dc6c15b8e313cc92c210c26c
diegolesmc@gmail.com <diegolesmc@gmail.com>
2024-02-22 13:33:32 +0100

    lista de objetos

---
+++ listadeobjetosjava/src/App.java
@@ -0,0 +1,17 @@
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+
+        List<Automovil> auto = new LinkedList<>(); 
+    
+        Automovil ferrari = new Automovil("Ferrari", "Rojo");
+        Automovil nissan = new Automovil("Skyline", "Azul");
+    
+        auto.add(ferrari); 
+        auto.add(nissan);
+        System.out.println(" " + auto.get(0));
+    }
+}
---
+++ listadeobjetosjava/src/Automovil.java
@@ -0,0 +1,24 @@
+
+public class Automovil {
+
+    private String marca;
+    private String color;
+
+    public Automovil(String marca, String color) {
+        this.marca = marca;
+        this.color = color;
+    }
+
+    public String toString() {
+        return "Automovil{" +
+                "marca='" + marca + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+    public String toString() {
+        return "Automovil{" +
+                "marca='" + marca + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
